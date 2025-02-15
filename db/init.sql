981a1311806c934febad49e5eca5c76059b70731
luizabarros <thecoderana@gmail.com>
2025-02-14 23:26:02 -0300

    feat: init project

---
+++ db/init.sql
@@ -0,0 +1,73 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS subject;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS professor_subject;
+DROP TABLE IF EXISTS class_subject;
+DROP TABLE IF EXISTS lecture;
+DROP TABLE IF EXISTS professor;
+DROP TABLE IF EXISTS class;
+DROP TABLE IF EXISTS day_of_week;
+
+CREATE TABLE subject (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    class_id UUID REFERENCES class(id) ON DELETE SET NULL,
+    professor_id UUID REFERENCES professor(id) ON DELETE SET NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE room (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE professor_subject (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    professor_id UUID REFERENCES professor(id) ON DELETE CASCADE,
+    subject_id UUID REFERENCES subject(id) ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE class_subject (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    class_id UUID REFERENCES class(id) ON DELETE CASCADE,
+    professor_id UUID REFERENCES professor(id) ON DELETE SET NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE lecture (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    subject_id UUID REFERENCES subject(id) ON DELETE CASCADE,
+    room_id UUID REFERENCES room(id) ON DELETE SET NULL,
+    day_of_week_id UUID REFERENCES day_of_week(id) ON DELETE SET NULL,
+    hour_init TIMESTAMP WITH TIME ZONE NOT NULL,
+    duration INTEGER NOT NULL
+);
+
+CREATE TABLE professor (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE class (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE day_of_week (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL
+);
